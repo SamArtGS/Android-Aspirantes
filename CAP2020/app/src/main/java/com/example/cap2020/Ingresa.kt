7fb2705c1ad201d7e167b1f3392260807c08dda4
SamArtGS <samuelarturo@icloud.com>
2020-04-19 16:37:28 -0500

    fix: Presentación 1

--- CAP2020/app/src/main/java/com/example/cap2020/Ingresa.kt
+++ CAP2020/app/src/main/java/com/example/cap2020/Ingresa.kt
@@ -8,5 +8,7 @@ class Ingresa : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ingresa)
+        private lateinit var auth: FirebaseAuth// ...
+        auth = FirebaseAuth.getInstance()
     }
 }
