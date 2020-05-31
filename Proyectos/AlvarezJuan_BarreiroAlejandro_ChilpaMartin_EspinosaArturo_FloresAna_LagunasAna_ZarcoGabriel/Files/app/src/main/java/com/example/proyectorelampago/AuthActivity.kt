e5b2f331080944a6c334b4804c42e495c8d6b2b4
Reus <artoby99@hotmail.com>
2020-05-31 11:17:06 -0500

    Agregación de proyecto

---
+++ Proyectos/AlvarezJuan_BarreiroAlejandro_ChilpaMartin_EspinosaArturo_FloresAna_LagunasAna_ZarcoGabriel/Files/app/src/main/java/com/example/proyectorelampago/AuthActivity.kt
@@ -0,0 +1,112 @@
+    package com.example.proyectorelampago
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_auth.*
+
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        // Splash screen
+        setTheme(R.style.AppTheme)
+
+        // Instanciacion de la app
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+        // Analytics events
+        val analytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message", "Integracion de Firebase completa")
+        analytics.logEvent("InitScreen", bundle)
+
+        // Setup
+        setup()
+    }
+
+    private fun setup() {
+        title = "Autenticación"
+
+        signInButton.setOnClickListener {
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
+
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                    "artoby99","246810").addOnCompleteListener {
+                        if(it.isSuccessful){
+                            showHome(it.result?.user?.email ?:"--", ProviderType.BASIC)
+                            //showAlertRegister()
+                        } else {
+                            showAlert()
+                        }
+                    }
+            } else {
+                showAlertNull()
+            }
+
+        }
+
+        logInButton.setOnClickListener {
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
+
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                    emailEditText.text.toString(), passwordEditText.text.toString()).addOnCompleteListener {
+                        if(it.isSuccessful){
+                            showHome(it.result?.user?.email ?:"--", ProviderType.BASIC)
+                        } else {
+                            showAlert()
+                        }
+                    }
+            } else {
+                showAlertNull()
+            }
+        }
+    }
+
+    private fun showAlert() {
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error de autentificación del usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+
+    }
+
+    private fun showAlertNull() {
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Ingrese todos los datos")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+
+    }
+
+    private fun showAlertRegister() {
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Confirmación")
+        builder.setMessage("Registro completado con éxito.")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+
+    }
+
+    private fun showHome(email: String, provider: ProviderType) {
+        val homeIntent = Intent(this, HomeActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+
+        startActivity(homeIntent)
+    }
+}
---
+++ Proyectos/AlvarezJuan_BarreiroAlejandro_ChilpaMartin_EspinosaArturo_FloresAna_LagunasAna_ZarcoGabriel/README.md
@@ -0,0 +1,34 @@
+[//]: <> (Color: #64e291 -> Para títulos)
+[//]: <> (Color: #614ad3 -> Para subtítulos)
+[//]: <> (Color: #eb4559 -> Para otro tipo de encabezados)
+
+# <span style="color:#64e291">Proyecto 1 de Android 📱</span>
+
+### <span style="color:#614ad3">Integrantes</span>
+
+- Alvarez Loran Juan Pablo 
+- Barreiro Valdez  Alejandro  
+- Chilpa Navarro Martín Enrique  
+- Espinosa Guadarrama Arturo  
+- Flores Salinas Ana Paula  
+- Lagunas Parra Ana Paola  
+- Zarco Muñoz Gabriel
+
+___
+
+# <span style="color:#eb4559">Ventana de login</span>
+
+<div class=text-justify>
+  Una vez inicializada la app en el dispositivo móvil, se mostrará una ventana la cual contendrá dos vistas de texto para que el 
+  usuario pueda ingresar sus datos para poder iniciar sesión, además de contar con dos botones, los cuales serán utilizados para 
+  confirmar el inicio de sesión o registrar a un nuevo usuario en caso de no existir (todos los usuarios creados serán
+  almacenados en una base de datos empleando la plataforma Firebase, más adelante se explica sus usos).<br>
+  
+  Tambien es posible registrarse o iniciar sesión con los tres botones que aparen debajo de estos últimos, los cuales se 
+  redireccionaran a la red social dependiendo del botón escogido (siendo Facebook, Google y Twitter las opciones), obteniendo la 
+  información del usuario con la ayuda de Firebase.
+</div>
+
+![Imagen de ventana de inicio](Imagenes/ventana-inicio.jpg?raw=true "Ventana de inicio de sesión")
+
+  
