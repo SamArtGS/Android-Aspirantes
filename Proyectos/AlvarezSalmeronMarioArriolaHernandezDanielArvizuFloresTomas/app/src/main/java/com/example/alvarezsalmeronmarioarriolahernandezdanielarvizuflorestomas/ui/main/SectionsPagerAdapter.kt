3653a08e66adde0aa042766822f8f59b29924036
TomasTAF <tomas@ciencias.unam.mx>
2020-05-30 19:25:58 -0500

    Se puede cambiar entre pantallas sin mover el látigo

--- Proyectos/AlvarezSalmeronMarioArriolaHernandezDanielArvizuFloresTomas/app/src/main/java/com/example/alvarezsalmeronmarioarriolahernandezdanielarvizuflorestomas/ui/main/SectionsPagerAdapter.kt
+++ Proyectos/AlvarezSalmeronMarioArriolaHernandezDanielArvizuFloresTomas/app/src/main/java/com/example/alvarezsalmeronmarioarriolahernandezdanielarvizuflorestomas/ui/main/SectionsPagerAdapter.kt
@@ -4,6 +4,10 @@ import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.example.alvarezsalmeronmario_arriolahernandezdaniel_arvizuflorestomas.AzulActivity
+import com.example.alvarezsalmeronmario_arriolahernandezdaniel_arvizuflorestomas.IndigoActivity
+import com.example.alvarezsalmeronmario_arriolahernandezdaniel_arvizuflorestomas.RojoActivity
+import com.example.alvarezsalmeronmario_arriolahernandezdaniel_arvizuflorestomas.VerdeActivity
 import com.example.alvarezsalmeronmarioarriolahernandezdanielarvizuflorestomas.R
 
 private val TAB_TITLES = arrayOf(
@@ -21,15 +25,35 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
     override fun getItem(position: Int): Fragment {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1)
+        when(position){
+            0 -> {
+                return AzulActivity()
+            }
+            1 -> {
+                return IndigoActivity()
+            }
+            2 -> {
+                return RojoActivity()
+            }
+            3 -> {
+                return VerdeActivity()
+            }
+            else -> return RojoActivity()
+        }
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
-        return context.resources.getString(TAB_TITLES[position])
+        return when(position) {
+            0 -> "AZUL"
+            1 -> "ÍNDIGO"
+            2 -> "ROJO"
+            3 -> "VERDE"
+            else -> null
+        }
     }
 
     override fun getCount(): Int {
         // Show 2 total pages.
-        return 2
+        return 4
     }
 }
