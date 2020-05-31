d1cb4f7ffbcf64fac3ef7ed5225be6f37304a0b9
analagunas <aplagunitas@gmail.com>
2020-05-31 14:50:41 -0500

    Se agrego el archivo Register

---
+++ Proyectos/AlvarezJuan_BarreiroAlejandro_ChilpaMartin_EspinosaArturo_FloresAna_LagunasAna_ZarcoGabriel/Files/app/src/main/java/com/example/proyectorelampago/register.kt
@@ -0,0 +1,12 @@
+package com.example.proyectorelampago
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class register : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
