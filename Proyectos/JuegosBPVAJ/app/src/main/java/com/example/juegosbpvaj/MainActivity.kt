300a0fa34cbe6a6fa955ddf9751cd49667be2eca
abrahamx9-r <abraham_g_r@ciencias.unam.mx>
2021-01-10 21:25:44 -0600

    menu principal creado

--- Proyectos/JuegosBPVAJ/app/src/main/java/com/example/juegosbpvaj/MainActivity.kt
+++ Proyectos/JuegosBPVAJ/app/src/main/java/com/example/juegosbpvaj/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.juegosbpvaj
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        juego1.setOnClickListener {
+            val intento1= Intent(this,juego1::class.java)
+                startActivity(intento1)
+            }
+        }
+
     }
+
 }
---
+++ Proyectos/JuegosBPVAJ/app/src/main/java/com/example/juegosbpvaj/juego1.kt
@@ -0,0 +1,11 @@
+package com.example.juegosbpvaj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class juego1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_juego1)
+    }
+}
