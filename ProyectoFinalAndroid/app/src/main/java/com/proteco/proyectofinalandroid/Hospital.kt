82a92c774b8be721266e8b455279d081ea40bb39
erick <rodreri@gmail.com>
2021-01-19 22:23:39 -0600

    añadi los mapas

--- ProyectoFinalAndroid/app/src/main/java/com/proteco/proyectofinalandroid/Hospital.kt
+++ ProyectoFinalAndroid/app/src/main/java/com/proteco/proyectofinalandroid/Hospital.kt
@@ -1,11 +1,18 @@
 package com.proteco.proyectofinalandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_hospital.*
 
 class Hospital : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hospital)
+
+        btn_Hos.setOnClickListener {
+            val intent : Intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- ProyectoFinalAndroid/app/src/main/java/com/proteco/proyectofinalandroid/Oxigeno.kt
+++ ProyectoFinalAndroid/app/src/main/java/com/proteco/proyectofinalandroid/Oxigeno.kt
@@ -1,11 +1,18 @@
 package com.proteco.proyectofinalandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_oxigeno.*
 
 class Oxigeno : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_oxigeno)
+
+        btn_Ox.setOnClickListener {
+            val intent : Intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
