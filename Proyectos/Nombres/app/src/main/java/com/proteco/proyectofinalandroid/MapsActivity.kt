d7ce81493f941f92e4c056163b2a64243048379b
Mauricio GS <mauricaz@outlook.com>
2021-01-31 17:47:09 -0600

    Agrego marcador al mapa y estilo

--- Proyectos/Nombres/app/src/main/java/com/proteco/proyectofinalandroid/MapsActivity.kt
+++ Proyectos/Nombres/app/src/main/java/com/proteco/proyectofinalandroid/MapsActivity.kt
@@ -12,6 +12,8 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
@@ -28,22 +30,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val cdmx = LatLng(19.4326296, -99.1331785)
+        mMap.addMarker(MarkerOptions().position(cdmx).title("Ciudad de México").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cdmx, 11F))
+
+
     }
 }
