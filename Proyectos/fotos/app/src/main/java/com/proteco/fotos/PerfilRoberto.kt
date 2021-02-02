4ceb624b33721387b8132de5c8effafbcfffd362
RobertoCarrichi <robertocarrichiunam@gmail.com>
2021-02-02 04:50:42 -0600

    feat: Creación y estructura inicial de la actividad que será llamada para la muestra de información.

---
+++ Proyectos/fotos/app/src/main/java/com/proteco/fotos/PerfilRoberto.kt
@@ -0,0 +1,21 @@
+package com.proteco.fotos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PerfilRoberto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil_roberto)
+
+        /*
+        * Aquí se encontrará el contenido del perfil en donde se podrá ver:
+        *  - Fotografía
+        *  - Nombre
+        *  - Pequeña descripción
+        *  - Perfil de GitHun
+        *  - Correo electrónico
+        *
+        * */
+    }
+}
